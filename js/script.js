e53d78dfa9fd3e903e422897291d3d98818ab6a4
ncutixavier <ncuti65@gmail.com>
2020-01-14 11:23:02 +0200

    Sport website Template

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function hideSearch() {
+    var x = document.getElementById("searchInput");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function hideNav() {
+    var x = document.getElementById("navbar");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
